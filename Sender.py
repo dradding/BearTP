de2436ae232bafd41f51455ccc7d68e71cc1fdb2
Daniel Radding <dradding@gmail.com>
2014-10-28 17:44:55 -0700

    rough draft

--- Sender.py
+++ Sender.py
@@ -12,18 +12,67 @@ class Sender(BasicSender.BasicSender):
         super(Sender, self).__init__(dest, port, filename, debug)
         if sackMode:
             raise NotImplementedError #remove this line when you implement SACK
+        self.file_len = len(self.infile)
+        self.window_open = 0
+        self.window_close = 0
+        self.current_ack = [0,0] #[ack, number of times recieved]
+        self.finished = False
+        self.last_packet = self.file_len/1472
 
     # Main sending loop.
     def start(self):
-        raise NotImplementedError
+
+        if self.file_len > 1471:
+            msg = self.infile.read(1472)
+        else:
+            msg = self.infile.read(self.file_len)
+
+        packet = self.make_packet("start",1,msg)
+        self.send(packet)
+
+        while not self.finished:
+            
+            while self.window_close - self.window_open < 4: #Fills out window
+                self.window_close += 1
+                if self.window_close == self.last_packet:
+                    msg = self.infile.read(self.file_len%1472)
+                    packet = self.make_packet("end",self.window_close,msg)
+                    self.send(packet)
+                elif self.window_close<self.last_packet:
+                    msg = self.infile.read(self.file_len)
+                    packet = self.make_packet("data",self.window_close,msg)
+                    self.send(packet)
+                else:
+                    #trigger resend?
+
+            rcv = self.recieve(500)
+            if rcv = None:
+                self.handle_timeout()
+            else:
+                if rcv == self.last_packet:
+                    self.finished = True
+
+                elif self.current_ack[0] == rcv:
+                    self.handle_dup_ack()
+                    
+                else:
+                    self.handle_new_ack()
+                    
+
+        #raise NotImplementedError
 
     def handle_timeout(self):
+        #resend
         pass
 
     def handle_new_ack(self, ack):
-        pass
+        self.window_open = 106
+        self.current_ack = [rcv, 1]
 
     def handle_dup_ack(self, ack):
+        self.current_ack[1] += 1
+        if self.current_ack[1] == 4:
+            #trigger resend
         pass
 
     def log(self, msg):
