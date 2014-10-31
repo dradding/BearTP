ff561b4a6a979cd9a236ed5b71b550aead130408
Daniel Radding <dradding@gmail.com>
2014-10-30 23:33:50 -0700

    passes first two tests

--- Sender.py
+++ Sender.py
@@ -8,7 +8,7 @@ import BasicSender
 This is a skeleton sender class. Create a fantastic transport protocol here.
 '''
 class Sender(BasicSender.BasicSender):
-    def __init__(self, dest, port, filename, debug=False, sackMode=False):
+    def __init__(self, dest, port, filename, debug=True, sackMode=False):
         super(Sender, self).__init__(dest, port, filename, debug)
         if sackMode:
             raise NotImplementedError #remove this line when you implement SACK
@@ -22,52 +22,66 @@ class Sender(BasicSender.BasicSender):
 
     # Main sending loop.
     def start(self):
-        message = "start"
+        message = "data"
         seqno = 0
-        data = self.infile.read(1472)
         #if (max_seqno == 0): #the size of the file can be handled in one send call
             #data = self.infile.read(self.file_len % 1472)
-        if (len(data) < 1472):
-            new_packet = self.make_packet(message, seqno, data)
-            self.send(new_packet)
-            response = self.receive(500)
-            handle_response(response)
+        new_packet = self.make_packet("start", seqno, "")
+        self.send(new_packet)
+        self.window[0] = new_packet
+        response = self.receive(.5)
+        # while self.handle_response(response) == False:
+        #     self.send(new_packet)
+        #     response = self.receive(.5)
+        # print "start packet recieved"
 
-        else:
             #data = self.infile.read(1472)
-            while not message == "end":
-                for i in range(self.window_base, self.window_max + 1):
-                    # if i != max_seqno:
-                    #     new_packet = self.make_packet(message, seqno, data)
-                    #     window[i] = new_packet
-                    #     self.send(new_packet)
+        while not message == "end":
+            for i in range(self.window_base, self.window_max + 1):
+                # if i != max_seqno:
+                #     new_packet = self.make_packet(message, seqno, data)
+                #     window[i] = new_packet
+                #     self.send(new_packet)
 
-                    if not i  in self.window.keys():
-                        new_packet = self.make_packet(message, i, data)
-                        self.window[i] = new_packet
-                        self.send(new_packet)
-                        data = self.infile.read(1472)
-                    if (len(data) < 1472):
+                if not i in self.window.keys():
+                    #print i
+                    data = self.infile.read(1400)
+                    if (len(data) < 1400):
                         message = "end"
-                    else:
-                        message = "data"
-                response = self.receive(500)
-                self.handle_response(response)
+                        #if end, send end, otherwise send data
+                    new_packet = self.make_packet(message, i, data)
+                    self.window[i] = new_packet
+                    #print data
+                    self.send(new_packet)
+                if message == "end":
+                    break       
+            #print self.window
+            response = self.receive(.5)
+            self.handle_response(response)
+
+        while len(self.window) != 0:
+            response = self.receive(.5)
+            self.handle_response(response)
+
 
     def handle_response(self, response_packet):
         if response_packet == None:           
             self.handle_timeout()
+            # return False
         else:
             if Checksum.validate_checksum(response_packet):
                 msg_type, seqno, data, checksum = self.split_packet(response_packet)
                 seqno = int(seqno)
+                print seqno
                 if seqno > self.current_ack[0]:
                     self.handle_new_ack(seqno)
                 if seqno == self.current_ack[0]:
                     self.handle_dup_ack(seqno)
+            # return True
 
     def handle_timeout(self):
         self.send(self.window[self.window_base])
+        print "resend due to timeout"
 
     def handle_new_ack(self, ack):
         self.window_base = ack
@@ -79,11 +93,16 @@ class Sender(BasicSender.BasicSender):
                 to_delete.append(key)
         for key in to_delete:
             del self.window[key]
+        '''if ack in self.window.keys():
+            self.send(self.window[ack])
+            response = self.receive(.5)
+            self.handle_response(response)'''
 
     def handle_dup_ack(self, ack):
         self.current_ack[1] += 1
-        if self.current_ack[1] == 3:
-            self.send(self.window[self.window_base])
+        if self.current_ack[1] >= 3:
+            self.send(self.window[ack])
+            print "resend due to dup ack"
 
     def log(self, msg):
         if self.debug:
