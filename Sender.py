63c2589dad8870845fec9fdfebff0c5c4de18c82
Daniel Radding <dradding@gmail.com>
2014-11-01 18:42:44 -0700

    cleaned up code

--- Sender.py
+++ Sender.py
@@ -8,11 +8,8 @@ import BasicSender
 This is a skeleton sender class. Create a fantastic transport protocol here.
 '''
 class Sender(BasicSender.BasicSender):
-    def __init__(self, dest, port, filename, debug=True, sackMode=True):
+    def __init__(self, dest, port, filename, debug=True, sackMode=False):
         super(Sender, self).__init__(dest, port, filename, debug)
-        # if sackMode:
-        #     raise NotImplementedError #remove this line when you implement SACK
-        #self.file_len = len(self.infile)
         self.sackMode = sackMode
         self.window_base = 0
         self.window_max = 4
@@ -27,38 +24,21 @@ class Sender(BasicSender.BasicSender):
     def start(self):
         message = "data"
         seqno = 0
-        #if (max_seqno == 0): #the size of the file can be handled in one send call
-            #data = self.infile.read(self.file_len % 1472)
         new_packet = self.make_packet("start", seqno, "")
         self.send(new_packet)
         self.window[0] = new_packet
         response = self.receive(.5)
-        # while self.handle_response(response) == False:
-        #     self.send(new_packet)
-        #     response = self.receive(.5)
-        # print "start packet recieved"
-
-            #data = self.infile.read(1472)
         while not message == "end":
             for i in range(self.window_base, self.window_max + 1):
-                # if i != max_seqno:
-                #     new_packet = self.make_packet(message, seqno, data)
-                #     window[i] = new_packet
-                #     self.send(new_packet)
-
                 if not i in self.window.keys():
-                    #print i
                     data = self.infile.read(1400)
                     if (len(data) < 1400):
                         message = "end"
-                        #if end, send end, otherwise send data
                     new_packet = self.make_packet(message, i, data)
                     self.window[i] = new_packet
-                    #print data
                     self.send(new_packet)
                 if message == "end":
                     break       
-            #print self.window
             response = self.receive(.5)
             self.handle_response(response)
 
@@ -66,23 +46,19 @@ class Sender(BasicSender.BasicSender):
             response = self.receive(.5)
             self.handle_response(response)
 
-
     def handle_response(self, response_packet):
         if response_packet == None:           
             self.handle_timeout()
-            # return False
         if Checksum.validate_checksum(response_packet):
             msg_type, seqno, data, checksum = self.split_packet(response_packet)
             if self.sackMode:
                 self.handle_sack(seqno)
             else:
                 seqno = int(seqno)
-                #print seqno
                 if seqno > self.current_ack[0]:
                     self.handle_new_ack(seqno)
                 if seqno == self.current_ack[0]:
                     self.handle_dup_ack(seqno)
-            # return True
 
     def handle_sack(self, sack):
         info = sack.split(";")
@@ -93,16 +69,11 @@ class Sender(BasicSender.BasicSender):
             sacks = info[1].split(",")
             sacks = map(int, sacks)
             self.current_sack = sacks
-        print info
         self.current_cum_ack = int(info[0])
         if self.current_cum_ack in self.window:
-            print "current_cum_ack request met!"
             self.send(self.window[self.current_cum_ack])
         self.window_base = self.current_cum_ack
         self.window_max = self.current_cum_ack + 4
-        print "window_base: "
-        print self.window_base
-        print self.window.keys()
         if len(self.current_sack)> 1:
             for key in self.window:
                 if not key in self.current_sack:
@@ -118,11 +89,9 @@ class Sender(BasicSender.BasicSender):
         if self.sackMode:
             for i in self.window:
                 if (not i in self.current_sack):
-                    print "Sack timeout handled"
                     self.send(self.window[i])
         else:
             self.send(self.window[self.window_base])
-        print "resend due to timeout"
 
     def handle_new_ack(self, ack):
         self.window_base = ack
@@ -134,22 +103,15 @@ class Sender(BasicSender.BasicSender):
                 to_delete.append(key)
         for key in to_delete:
             del self.window[key]
-        '''if ack in self.window.keys():
-            self.send(self.window[ack])
-            response = self.receive(.5)
-            self.handle_response(response)'''
 
     def handle_dup_ack(self, ack):
         self.current_ack[1] += 1
         if self.current_ack[1] >= 4:
             self.send(self.window[ack])
-            print "resend due to dup ack"
 
     def log(self, msg):
         if self.debug:
             print msg
-
-
 '''
 This will be run if you run this script from the command line. You should not
 change any of this; the grader may rely on the behavior here to test your
